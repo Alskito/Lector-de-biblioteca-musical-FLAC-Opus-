f3f8f3bbce855fc2b3dabc9a5eba45734e0ded9b
Alskito <zamudioalexander2@gmail.com>
2026-09-25 12:59:17 -0600

    metadatos.hpp

--- metadatos.hpp
+++ metadatos.hpp
@@ -3,6 +3,4 @@
 #include <filesystem>
 #include "cancion.hpp"
  
-// Recorre una carpeta (y subcarpetas) y devuelve todas las canciones
-// .flac / .opus encontradas, ya con sus metadatos leídos.
 std::vector<Song> escanearCarpeta(const std::filesystem::path& carpeta);
